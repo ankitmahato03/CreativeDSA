b812b645cec3177d84278851a0d5b6e3b15c4c51
ankitmahato03 <ankitmahato1212@gmail.com>
2025-03-15 21:26:34 +0530

    90 days Challenge

---
+++ pattern1.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+void pattern(int n)
+
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+}
+
+void print3(int n)
+{
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+void print4(int n)
+{
+    int i = 0;
+    for (int j = 0; j <= i; j++)
+    {
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    print4(n);
+}
