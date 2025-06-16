5ae0c3cf1a62f08d3c3d3c8008e235965ad1cd01
Ankit Mahato <101471279+ankitmahato03@users.noreply.github.com>
2025-06-16 05:43:31 +0000

    cpp

--- pattern1.cpp
+++ pattern1.cpp
@@ -126,10 +126,42 @@ void reversep(int n){
     }
 }
 
+
+void pattern7(int n){
+    for (int i =0 ; i<n;i++){
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        for( int j=0 ; j<=2*n-(2*i+1);j++){
+            cout << "*";
+        }
+
+          for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+
+
+void pattern8(int n){
+    for(int i =1 ; i<=2*n-1;i++){
+        int stars=i;
+        if(i>n ) stars=2*n-i;
+        for(int j=1; j<=stars;j++){
+
+            cout<< "*";
+        }
+        cout << endl;
+
+    }
+}
+
 int main()
 {
     int n;
     cout << "Enter the number: " ;
     cin >>n;
-    reversep(n);
+    pattern8(n);
 }
