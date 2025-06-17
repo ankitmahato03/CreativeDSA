ff86db2139bedc82f11c87b4a42861ed3b7b6c48
Ankit Mahato <101471279+ankitmahato03@users.noreply.github.com>
2025-06-17 07:17:46 +0000

    massage

--- pattern1.cpp
+++ pattern1.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 void pattern(int n)
-
 {
     for (int i = 0; i < n; i++)
     {
@@ -161,22 +160,57 @@ void pattern8(int n){
 
 void pattern9(int n){
     int start=1;
-for(int i=0; i<n;i++){
+    for(int i=0; i<n;i++){
     if(i%2==0){
-        start=1;
+        start=1 ;
     }
     else {
         start=0;
     }
 
-    for(int j=0;j<i;j++){
+    for(int j=0;j<=i;j++){
         cout<<start;
         start=1-start;
     }
 
     cout<<endl;
 
-}    
+    }    
+}
+
+
+void pattern10(int n){
+    int space= 2*(n-1);
+    for (int i=1;i<=n;i++){
+        //numbers
+        for(int j=1;j<=i;j++){
+            cout<< j;
+        }
+        //spaces
+        for(int j=1;j<=space;j++){
+            cout<<" ";
+        }
+        //NUmbers
+        for(int j=i;j>=1;j--){
+            cout<<j;
+        }
+        cout<<endl;
+        space -=2;
+    }
+}
+
+void pattern11(int n){
+    int num=1;
+    for (int i=0;i<=n;i++){
+        for(int j=0;j<i;j++){
+            cout<< num << " ";
+            num=num+1;
+        }
+
+
+
+        cout<<endl;
+    }
 }
 
 int main()
@@ -184,5 +218,5 @@ int main()
     int n;
     cout << "Enter the number: " ;
     cin >>n;
-    pattern9(n);
+    pattern11(n);
 }
