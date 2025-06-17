ba4c0e1dc0a7b9d2f1056541de0d41e6e1310a57
Ankit Mahato <101471279+ankitmahato03@users.noreply.github.com>
2025-06-17 05:49:20 +0000

    pattern 9

--- pattern1.cpp
+++ pattern1.cpp
@@ -158,10 +158,31 @@ void pattern8(int n){
     }
 }
 
+
+void pattern9(int n){
+    int start=1;
+for(int i=0; i<n;i++){
+    if(i%2==0){
+        start=1;
+    }
+    else {
+        start=0;
+    }
+
+    for(int j=0;j<i;j++){
+        cout<<start;
+        start=1-start;
+    }
+
+    cout<<endl;
+
+}    
+}
+
 int main()
 {
     int n;
     cout << "Enter the number: " ;
     cin >>n;
-    pattern8(n);
+    pattern9(n);
 }
