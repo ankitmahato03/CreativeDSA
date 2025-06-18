0c035970a7425e1b51dfad05daee0fcc69f2ddda
Ankit Mahato <101471279+ankitmahato03@users.noreply.github.com>
2025-06-18 09:00:25 +0000

    Patterns are solved

--- pattern1.cpp
+++ pattern1.cpp
@@ -347,6 +347,33 @@ void pattern18(int n){
         else space +=2;
     }
 }
+void pattern19(int n){
+    for (int i=0;i< n;i++){
+        for(int j=0;j<n;j++){
+            if(i==0 || j==0 || i==n-1 || j==n-1){ 
+                cout <<"*";
+            }
+            else cout <<" ";
+        }
+ 
+        cout<<endl;
+    }
+}
+
+
+void pattern20(int n){
+    for(int i=0;i<2*n-1;i++){
+        for(int j=0;j<2*n-1;j++){
+            int top=i;
+            int left=j;
+            int right=(2*n-2)-j;
+            int down = (2*n-2)-i;
+            cout<<(n- min(min(top,down),min(left,right))) << " ";
+
+        }
+        cout<<endl;
+    }
+}
 
 
 int main()
@@ -354,5 +381,5 @@ int main()
     int n;
     cout << "Enter the number: " ;
     cin >>n;
-    pattern18(n);
+    pattern20(n);
 }
